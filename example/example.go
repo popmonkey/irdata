990ba73b36db84afa4a299b156d94832815b8d38
Jules Cisek <popmonkey+github.irdata@gmail.com>
2024-06-08 14:15:17 -0700

    improved example
    shows structured and unstructured marshalling examples

--- example/example.go
+++ example/example.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"time"
 
 	"github.com/popmonkey/irdata"
@@ -52,18 +53,76 @@ func main() {
 		log.Panic(err)
 	}
 
+	var foo map[string]interface{}
+
+	json.Unmarshal(data, &foo)
+
+	// structured unmarshall
+	type license struct {
+		Category string  `json:"category_name"`
+		License  string  `json:"group_name"`
+		SR       float64 `json:"safety_rating"`
+		IR       float64 `json:"irating"`
+		CPI      float64 `json:"cpi"`
+	}
+
 	var member struct {
-		CustID      int64  `json:"cust_id"`
-		DisplayName string `json:"display_name"`
-		MemberSince string `json:"member_since"`
+		CustID      int64              `json:"cust_id"`
+		DisplayName string             `json:"display_name"`
+		MemberSince string             `json:"member_since"`
+		LastLogin   string             `json:"last_login"`
+		Licenses    map[string]license `json:"licenses"`
 	}
 
 	if err := json.Unmarshal(data, &member); err != nil {
 		log.Panic(err)
 	}
 
+	data, err = i.GetWithCache("data/series/seasons", time.Duration(1)*time.Hour)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// unstructured unmarshall
+	var seasons []map[string]interface{}
+
+	if err := json.Unmarshal(data, &seasons); err != nil {
+		log.Panic(err)
+	}
+
 	fmt.Print("\n\nMember Info:\n")
-	fmt.Printf("\tname:\t%s\n", member.DisplayName)
-	fmt.Printf("\tid:\t%d\n", member.CustID)
-	fmt.Printf("\tsince:\t%s\n", member.MemberSince)
+	fmt.Printf("\tname:      %s\n", member.DisplayName)
+	fmt.Printf("\tid:        %d\n", member.CustID)
+	fmt.Printf("\tsince:     %s\n", member.MemberSince)
+	fmt.Printf("\tlast seen: %s\n\n", member.LastLogin)
+
+	keys := make([]string, len(member.Licenses))
+	for k := range member.Licenses {
+		keys = append(keys, k)
+	}
+
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		license := member.Licenses[k]
+		fmt.Printf("\t%s: %s\n\t\tIR: %0.0f\tSR: %0.2f\t[CPI: %0.3f]\n",
+			license.Category,
+			license.License,
+			license.IR,
+			license.SR,
+			license.CPI,
+		)
+	}
+
+	fmt.Print("\n--- Current Series ---\n\n")
+
+	for _, season := range seasons {
+		fmt.Printf("[%5.0f | %5.0f] %s\n",
+			season["series_id"].(float64),
+			season["season_id"].(float64),
+			season["season_name"],
+		)
+	}
+
+	fmt.Print("\n\n")
 }
